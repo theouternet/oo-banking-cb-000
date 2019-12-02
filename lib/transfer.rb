aaf6c05024240ab0bffcb80d512f64e0cdbab1ab
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-12-02 00:35:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
 
-attr_accessor :status, :sender, :receiver
+attr_accessor :status, :sender, :receiver, :amount
 
 
 
 
-def initialize(sender, receiver, status="pending")
+def initialize(sender, receiver, status="pending", amount=50)
+  @amount = amount
   @status = status
   @sender = sender
   @receiver = receiver
