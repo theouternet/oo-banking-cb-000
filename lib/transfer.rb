486dc4dc7a3496ff57bdc863564c4a5867345153
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-12-02 00:35:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
 
+attr_accessor :status, :sender, :receiver
 
 
 
 
-
-def initialize(sender, receiver)
+def initialize(sender, receiver, status="pending")
+  @status = status
   @sender = sender
   @receiver = receiver
 
